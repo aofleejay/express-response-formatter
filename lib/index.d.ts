eb557858f66d1318b7e5c0ecafcaa2ef830194ef
aofleejay <aofleejay@hotmail.com>
2020-03-06 03:36:12 +0700

    :green_heart: Manualy publish.

---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express'
+interface LooseObject {
+  [key: string]: any
+}
+declare global {
+  namespace Express {
+    interface Response {
+      formatter: LooseObject
+    }
+  }
+}
+declare const responseEnhancer: () => (
+  req: Request<import('express-serve-static-core').ParamsDictionary>,
+  res: Response<any>,
+  next: NextFunction,
+) => void
+export default responseEnhancer
---
+++ lib/methods.d.ts
@@ -0,0 +1,9 @@
+interface Method {
+  name: string
+  code: string
+  message: string
+  isSuccess: boolean
+}
+declare const methods: Method[]
+export { Method }
+export default methods
